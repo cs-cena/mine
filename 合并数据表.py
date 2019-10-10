8e3e3eec4bf8e59beee398ebac935c3326a3ccf5
cs-cena <49243060+cs-cena@users.noreply.github.com>
2019-10-10 17:12:00 +0800

    Update 合并数据表.py

--- 合并数据表.py
+++ 合并数据表.py
@@ -10,11 +10,10 @@ if __name__ == '__main__':
     
     path_source = r"C:\Users\***\Desktop\shandong\ss"
     
-    for i in os.walk(path_source):
-        for file in i[2]:
-            path = r"C:\Users\***\Desktop\shandong\ss\%s" % file
-            df = pd.read_csv(path, names=["name","id","item_id","court","datetime","id","page"])
-            dfs.append(df)
+    for i in os.listdir(path_source):   	
+        path = r"C:\Users\chensheng\Desktop\shandong\ss\%s" % file
+        df = pd.read_csv(path, names=["姓名","证件号码","案件号","判决法院","公布时间","id","页数"])
+        dfs.append(df)
             
     df1 = pd.concat(dfs)
     df2 = df1[["name","id"]]
